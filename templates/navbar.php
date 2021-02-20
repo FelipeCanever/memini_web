8a50a149477b7fafa92715f08accd453118ee953
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-02-20 17:42:11 -0300

    Templatize index.php

---
+++ templates/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-dark bg-dark">
+	<div class="container-fluid">
+		<a href="#" class="navbar-brand"><?= TITLE ?></a>
+
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a href="#" class="nav-link">Entrar</a>
+			</li>
+		</ul>
+	</div>
+</nav>
