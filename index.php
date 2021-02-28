6843b00386102fae5e08e107dde2d51a7f74c13b
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-02-28 20:27:14 -0300

    Add breakpoints to decks

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
 			<?php foreach ($decks as $deck): ?>
 				<!-- Baralho -->
-				<div class="col-3">
+				<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 					<div class="card text-dark bg-light">
 						<div class="card-body">
 							<h5 class="card-title"><?= $deck["title"] ?></h5>
