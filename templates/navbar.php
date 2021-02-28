d7447ce8c45d226ada342d1256392d37222b7ed0
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-02-28 19:39:57 -0300

    Make navbar light

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg">
 	<div class="container-fluid">
 		<!-- Título -->
 		<a href="index.php" class="navbar-brand"><?= TITLE ?></a>
