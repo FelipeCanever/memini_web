f6bc1a0b98a09ff315553523176dec37b58abc50
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-01 14:31:01 -0300

    Restyle decks

--- templates/deck.php
+++ templates/deck.php
@@ -1,12 +1,33 @@
-<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-	<div class="card border-dark mb-4">
+<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 text-center">
+	<div class="card border-dark shadow">
 		<div class="card-body">
-			<h5 class="card-title"><?= $deck["title"] ?></h5>
-			<p class="card-text"><?= $deck["description"] ?></p>
+			<!-- Título -->
+			<h4 class="card-title"><?= $deck["title"] ?></h4>
+			<!-- Descrição -->
+			<p class="card-text lead"><?= $deck["description"] ?></p>
 
-			<a href="#" class="btn btn-sm btn-primary">Abrir</a>
-			<a href="#" class="btn btn-sm btn-secondary disabled">Estudar</a>
-			<a href="#" class="btn btn-sm btn-danger disabled">Excluir</a>
+			<!-- Botões -->
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12 mb-1">
+						<a href="#" class="btn btn-outline-primary btn-sm w-100 h-100">
+							Abrir
+						</a>
+					</div>
+
+					<div class="col-sm-12 mb-1">
+						<a href="#" class="btn btn-outline-secondary btn-sm w-100 h-100 disabled">
+							Estudar
+						</a>
+					</div>
+
+					<div class="col-sm-12 mb-1">
+						<a href="#" class="btn btn-outline-danger btn-sm w-100 h-100 disabled">
+							Excluir
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
