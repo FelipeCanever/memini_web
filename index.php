2b94ecba38ae9a8eb31ad300072368c64e85633e
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-01 12:51:51 -0300

    Create deck template

--- index.php
+++ index.php
@@ -41,23 +41,12 @@
 
 		<?php if (isset($_SESSION["username"])): ?>
 			<div class="row">
-				<?php require "data.php"; ?>
+				<?php
+					require "data.php";
 
-				<?php foreach ($decks as $deck): ?>
-					<!-- Baralho -->
-					<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-						<div class="card text-dark bg-light mb-4">
-							<div class="card-body">
-								<h5 class="card-title"><?= $deck["title"] ?></h5>
-								<p class="card-text"><?= $deck["description"] ?></p>
-
-								<a href="#" class="btn btn-sm btn-primary">Abrir</a>
-								<a href="#" class="btn btn-sm btn-secondary disabled">Estudar</a>
-								<a href="#" class="btn btn-sm btn-danger disabled">Excluir</a>
-							</div>
-						</div>
-					</div>
-				<?php endforeach; ?>
+					foreach ($decks as $deck)
+						require "templates/deck.php";
+				?>
 			</div>
 		<?php endif; ?>
 	</div>
---
+++ templates/deck.php
@@ -0,0 +1,12 @@
+<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+	<div class="card text-dark bg-light mb-4">
+		<div class="card-body">
+			<h5 class="card-title"><?= $deck["title"] ?></h5>
+			<p class="card-text"><?= $deck["description"] ?></p>
+
+			<a href="#" class="btn btn-sm btn-primary">Abrir</a>
+			<a href="#" class="btn btn-sm btn-secondary disabled">Estudar</a>
+			<a href="#" class="btn btn-sm btn-danger disabled">Excluir</a>
+		</div>
+	</div>
+</div>
