3a75b618a383557be26d85496b527411350bc6b5
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-01 14:36:57 -0300

    Restyle cards

--- templates/card.php
+++ templates/card.php
@@ -1,18 +1,22 @@
-<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-	<div class="card border-dark bg-light mb-4">
+<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4 text-center">
+	<div class="card border-dark shadow">
 		<div class="card-body">
 			<!-- Frente -->
-			<h5 class="card-title mt-5 mb-5 text-center"><?= $card["front"] ?></h5>
+			<h5 class="card-title mt-5 mb-5"><?= $card["front"] ?></h5>
 
 			<!-- Botões -->
 			<div class="container-fluid">
 				<div class="row">
-					<div class="col mb-1">
-						<a href="#" class="btn btn-outline-primary btn-sm w-100 h-100 disabled">Virar</a>
+					<div class="col-sm-12 mb-1">
+						<a href="#" class="btn btn-outline-primary btn-sm w-100 h-100 disabled">
+							Virar
+						</a>
 					</div>
 
-					<div class="col mb-1">
-						<a href="#" class="btn btn-outline-danger btn-sm w-100 h-100 disabled">Excluir</a>
+					<div class="col-sm-12 mb-1">
+						<a href="#" class="btn btn-outline-danger btn-sm w-100 h-100 disabled">
+							Excluir
+						</a>
 					</div>
 				</div>
 			</div>
