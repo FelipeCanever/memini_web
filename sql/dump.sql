c89ab0a310cdc80a6233b1f4e575324e0e10d121
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-11 10:59:45 -0300

    Write SQL for creating deck table

--- sql/dump.sql
+++ sql/dump.sql
@@ -5,3 +5,12 @@ CREATE TABLE `avii_desenvweb`.`user`(
 	
 	PRIMARY KEY	(`user_id`)
 );
+
+CREATE TABLE `avii_desenvweb`.`deck`(
+	`deck_id`	INT 			NOT NULL	AUTO_INCREMENT,
+	`user_id`	INT 			NOT NULL,
+	`title`		VARCHAR(255)	NOT NULL,
+	
+	PRIMARY KEY	(`deck_id`),
+	FOREIGN KEY	(`user_id`)	REFERENCES	`avii_desenvweb`.`user`	(`user_id`)
+);
