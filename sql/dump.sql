e37c55b436bcbfc345b96fe0d1bc21b734b1d4fb
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-11 11:05:42 -0300

    Write SQL for creating card table

--- sql/dump.sql
+++ sql/dump.sql
@@ -14,3 +14,22 @@ CREATE TABLE `avii_desenvweb`.`deck`(
 	PRIMARY KEY	(`deck_id`),
 	FOREIGN KEY	(`user_id`)	REFERENCES	`avii_desenvweb`.`user`	(`user_id`)
 );
+
+CREATE TABLE `avii_desenvweb`.`card`(
+	`card_id`			INT 			NOT NULL	AUTO_INCREMENT,
+
+	`user_id`			INT 			NOT NULL,
+	`deck_id`			INT 			NOT NULL,
+
+	`front`				VARCHAR(255)	NOT NULL,
+	`back`				VARCHAR(255)	NOT NULL,
+
+	`repetition_count`	INT 			NOT NULL,
+	`time_interval`		INT 			NOT NULL,
+	`ease_factor`		REAL 			NOT NULL,
+	
+	PRIMARY KEY	(`card_id`),
+
+	FOREIGN KEY	(`user_id`)	REFERENCES	`avii_desenvweb`.`user`	(`user_id`),
+	FOREIGN KEY	(`deck_id`)	REFERENCES	`avii_desenvweb`.`deck`	(`deck_id`)
+);
