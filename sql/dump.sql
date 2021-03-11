21f4aef95df6fa32a77633df4457a6b89eb144f1
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-11 10:55:42 -0300

    Write SQL for creating user table

---
+++ sql/dump.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `avii_desenvweb`.`user`(
+	`user_id`	INT 			NOT NULL	AUTO_INCREMENT,
+	`username`	VARCHAR(255)	NOT NULL,
+	`password`	VARCHAR(255)	NOT NULL,
+	
+	PRIMARY KEY	(`user_id`)
+);
