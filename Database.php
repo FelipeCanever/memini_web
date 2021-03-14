e6a3364253978c84d240f0a1dd681d579c9ebee6
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-14 14:14:49 -0300

    Implement selecting user from database

--- Database.php
+++ Database.php
@@ -1,5 +1,7 @@
 <?php
 
+require "models/User.php";
+
 class Database {
 	private static $database = "avii_desenvweb";
 	private $connection;
@@ -12,5 +14,23 @@ class Database {
 
 	public function __destruct() {
 		mysqli_close($this->connection);
+	}
+
+	public function selectUser($username, $password) {
+		$databaseName = Database::$database;
+
+		$result = mysqli_query(
+			$this->connection,
+
+			"SELECT `username`
+			FROM `$databaseName`.`user`
+			WHERE `username` = '$username' AND `password` = '$password';"
+		);
+
+		if ($result->num_rows <= 0)
+			return false;
+		
+		$user = mysqli_fetch_assoc($result);
+		return new User($user["username"]);
 	}
 }
