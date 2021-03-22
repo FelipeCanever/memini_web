a24fc8d8ab66e2799cad0f0bf2e863f2d4605836
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-22 20:44:51 -0300

    Include deck id in model

--- models/Deck.php
+++ models/Deck.php
@@ -1,10 +1,16 @@
 <?php
 
 class Deck {
+	private $deck_id;
 	private $title;
 
-	public function __construct($title) {
+	public function __construct($deck_id, $title) {
+		$this->deck_id = $deck_id;
 		$this->title = $title;
+	}
+
+	public function getDeckId() {
+		return $this->deck_id;
 	}
 
 	public function getTitle() {
